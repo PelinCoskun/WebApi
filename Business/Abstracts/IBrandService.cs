bce0047725d3b95cbd5d5b907a7a5bc2528c43c3
PelinCoskun <pelinefpit@gmail.com>
2024-02-25 22:36:24 +0300

    Add project files.

---
+++ Business/Abstracts/IBrandService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Entities.Concretes;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Dtos.Requests;
+using Business.Dtos.Responses;
+
+namespace Business.Abstracts;
+
+public interface IBrandService
+{
+    CreateBrandRequest Add(CreateBrandRequest createBrandRequest);
+    List<GetAllBrandResponse> GetAll();
+}
---
+++ Business/Concretes/BrandManager.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstracts;
+using Business.Dtos.Requests;
+using Business.Dtos.Responses;
+using DataAccess.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes;
+
+public class BrandManager : IBrandService
+{
+    IBrandDal _brandDal;
+
+    public BrandManager(IBrandDal brandDal)
+    {
+        _brandDal = brandDal;
+    }
+
+    public CreateBrandRequest Add(CreateBrandRequest createBrandRequest)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<GetAllBrandResponse> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+}
